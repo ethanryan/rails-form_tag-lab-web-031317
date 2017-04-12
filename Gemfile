43e4112f0a1ba5ee5aae1449574762d5d78af8ba
ethanryan <ethanryan@gmail.com>
2017-04-11 22:42:40 -0400

    done

--- Gemfile
+++ Gemfile
@@ -23,13 +23,17 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'web-console', group: :development #adding this
+
 group :development, :test do
+
   gem 'byebug'
-  gem 'web-console', '~> 2.0'
-  gem 'spring'
-  gem 'rspec-rails', '~> 3.0.0'
+# gem 'web-console', '~> 2.0'
+# gem 'spring'
+# REMEMBER THIS RSPEC_RAILS GEM VERSION
+  gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
+
   gem 'factory_girl_rails'
   gem 'capybara'
   gem 'database_cleaner'
 end
-
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
+  def create
+    #Post.create(title: params[:post][:title], description: params[:post][:description])
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    #above params need to be params[:first_name],
+    #not params[:student][:first_name]
+    redirect_to students_path #after creating new student, user is redirected to index page to see all students.
+  end
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
   end
 
 end
